27bd600b465f7ada3044763085bcdf32e848ab43
Shivendra Thakur <shivendrathakur321@outlook.com>
2024-05-16 21:48:08 +0530

    first commit

---
+++ lp-netflix/src/Components/header.jsx
@@ -0,0 +1,23 @@
+
+const Header = () => {
+    return (
+        <>
+
+            <div className='flex justify-between items-center mx-16'>
+                <div className='image px-12'> 
+                    <img src="./logoNetflix.png" width="250px" />
+                    {/* <div className='bg-[rgba(0,0,0,0.5)] over w-[100%] h-[100vh]'>
+
+                </div> */}
+
+                </div>
+              
+                <div className='button px-11'>
+                    <button className='btn px-5 py-2 rounded-md bg-red-600 text-white font-bold'>Sign in</button>
+                </div>
+            </div>
+        </>
+    )   
+}
+
+export default Header
---
+++ lp-netflix/src/Components/hero.jsx
@@ -0,0 +1,23 @@
+
+const Hero = () => {
+    return (
+        <div className='flex flex-col items-center h-screen mt-48 px-8 '>
+            <div className='text-center text-white'>
+                <h2 className='text-5xl font-extrabold'>Unlimited movies, TV shows, and more</h2>
+            </div>
+            <div className='text-center text-white text-2xl pt-5'>
+                <p>Watch anywhere. Cancel anytime.</p>
+            </div>
+            <div className='text-center text-white text-xl py-5'>
+                <p>Ready to watch? Enter your email or mobile number to create or restart your membership.</p>
+            </div>
+            <div className='text-center flex gap-3 w-1/2 justify-center'>
+            <input type='text' className='btn px-4 py-3  bg-transparent border-slate-300 border-2 w-1/2 text-white placeholder-gray-400 ' placeholder='Email or mobile number'/>
+            <button className='btn bg-red-700 text-white px-6 py-2 font-bold text-xl'>
+                    Get Started</button>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
